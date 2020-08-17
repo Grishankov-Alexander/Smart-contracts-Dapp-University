69d42aede41b40157e6ec87ef9964f0a25e3bd73
Grishankov_Alexander <grishankov.alexander@gmail.com>
2020-08-17 14:13:56 +0300

    Create Math.sol

---
+++ Math.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.5.1;
+
+library Math2 {
+    function multiply(uint256 a, uint256 b) 
+        internal
+        pure
+        returns(uint256)
+    {
+        return a * b;
+    }
+}
